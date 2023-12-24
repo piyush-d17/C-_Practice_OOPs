92a2fb9dae25ccafc4d82b399cb58f5bf1f0cc2c
Piyush Singh <105699619+piyush-d17@users.noreply.github.com>
2023-12-24 13:26:24 +0530

    polymorphism.cpp

--- polymorphism.cpp
+++ polymorphism.cpp
@@ -1,1 +1,25 @@
-#include<iostream>
+#include <iostream>
+using namespace std;
+class poly{
+    public:
+        void fun()
+        {
+            cout<<"I am a function with no argument"<<endl;
+        }
+        void fun(int x)
+        {
+            cout<<"I am a function with int argument"<<endl;
+        }
+        void fun(int x,int y)
+        {
+            cout<<"I am a function with double argument"<<endl;
+        }
+};
+int main()
+{
+    poly obj;
+    obj.fun();
+    obj.fun(5);
+    obj.fun(7,7);
+    return 0;
+}
